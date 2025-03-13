f130420f3c0aa6875d7bd9c02f0c1846bf85906c
wil <wiluniversityacc@gmail.com>
2025-03-13 14:57:14 +0700

    jadii gini cara pake input system

--- Assets/_Scripts/MovementScript.cs
+++ Assets/_Scripts/MovementScript.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float movementSpeed;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = moveInput * movementSpeed;
+        animator.SetBool("isWalking", moveInput != Vector2.zero);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
