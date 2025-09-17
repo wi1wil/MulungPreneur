83d34a855436b3c04aecb407c11d7d425d43baf9
wil <wiluniversityacc@gmail.com>
2025-09-17 22:46:03 +0700

    MulungPreneur V1.1
    Cleaned the scripts, created new scene to replace the old version of the game.

---
+++ Assets/V1/_Scripts/Done/InteractionDetectorScript.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionDetectorScript : MonoBehaviour
+{
+    private IInteractables InteractableInRange = null;
+    private ShopKeeperScript ShopKeeperInRange = null;
+    public GameObject interactionIcon;
+
+    void Start()
+    {
+        interactionIcon.SetActive(false);
+    }
+
+    public void OnShopInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (ShopKeeperInRange != null)
+            {
+                ShopKeeperInRange?.OpenShop();
+                Debug.Log("Shop interaction triggered");
+            }
+        }
+    }
+
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            InteractableInRange?.Interact();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out ShopKeeperScript shopKeeper))
+        {
+            Debug.Log("Entered shopkeeper range");
+            ShopKeeperInRange = shopKeeper;
+        }
+
+        if (collision.TryGetComponent(out IInteractables interactable) && interactable.canInteract())
+        {
+            InteractableInRange = interactable;
+            interactionIcon.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out ShopKeeperScript shopKeeper) && shopKeeper == ShopKeeperInRange)
+        {
+            Debug.Log("Exited shopkeeper range");
+            ShopKeeperInRange = null;
+        }
+
+        if (collision.TryGetComponent(out IInteractables interactable) && interactable == InteractableInRange)
+        {
+            InteractableInRange = null;
+            interactionIcon.SetActive(false);
+        }
+    }
+}
---
+++ Assets/V1/_Scripts/Done/SaveControllerScript.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Rendering;
+
+public class SaveControllerScript : MonoBehaviour
+{   
+    private string saveLocation;
+    private InventoryManagerScripts inventoryManager;
+    private WorldTime worldTime;
+    private VolumeSettings volumeSettings;
+    private VideoSettingsScript videoSettings;
+    private PlayerWalletScript playerWalletScript;
+    private MovementScript playerMovementScript;
+    private PlayerItemPickUpScript playerItemPickUpScript;
+    private Equipment equipmentScript;
+
+    [SerializeField] private float autosaveInterval = 30f; 
+
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+        inventoryManager = FindObjectOfType<InventoryManagerScripts>();
+        worldTime = FindObjectOfType<WorldTime>();
+        volumeSettings = FindObjectOfType<VolumeSettings>();
+        videoSettings = FindObjectOfType<VideoSettingsScript>();
+        playerWalletScript = FindObjectOfType<PlayerWalletScript>();
+        playerMovementScript = FindObjectOfType<MovementScript>();
+        playerItemPickUpScript = FindObjectOfType<PlayerItemPickUpScript>();
+        equipmentScript = FindObjectOfType<Equipment>();
+
+        LoadGame();
+        StartCoroutine(AutoSaveRoutine());
+    }
+
+    private IEnumerator AutoSaveRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(autosaveInterval);
+            SaveGame();
+        }
+    }
+
+    public void SaveGame()
+    {
+        Debug.Log("Saving quest progress: " + JsonUtility.ToJson(QuestManagerScript.Instance.activateQuests, true));
+        SaveDataScript saveData = new SaveDataScript
+        {
+            playerPosition = GameObject.Find("Player").transform.position,
+            mapBoundary = FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D.name,
+            inventorySaveData = inventoryManager.getInventoryItem(),
+            currentDay = worldTime.getCurrentDay(),
+            currentTimeTicks = worldTime.getCurrentTimeTicks(),
+            questProgressData = QuestManagerScript.Instance.activateQuests,
+            handinQuestsID = QuestManagerScript.Instance.handinQuests,
+            playerMoney = playerWalletScript.playerMoney,
+            bagLevel = equipmentScript.bagLevel,
+            inventorySize = inventoryManager.inventorySize,
+            footwearLevel = equipmentScript.footwearLevel,
+            playerSpeed = playerMovementScript.movementSpeed,
+            glovesLevel = equipmentScript.glovesLevel,
+            pickUpSpeed = playerItemPickUpScript.holdDuration,
+            toolLevel = equipmentScript.pickingToolLevel,
+            pickUpRange = playerItemPickUpScript.pickUpRange
+        };
+
+        // videoSettings.SaveSettings();
+        volumeSettings.LoadVolume();
+        string json = JsonUtility.ToJson(saveData, true);
+        Debug.Log("Saving JSON: " + json);
+        File.WriteAllText(saveLocation, json);
+        Debug.Log("Game Saved");
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            string json = File.ReadAllText(saveLocation);
+            Debug.Log("Loading JSON: " + json);
+            SaveDataScript saveData = JsonUtility.FromJson<SaveDataScript>(json);
+
+            GameObject.Find("Player").transform.position = saveData.playerPosition;
+            GameObject.FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+            inventoryManager.setInventoryItem(saveData.inventorySaveData);
+            worldTime.setWorldTime(saveData.currentDay, saveData.currentTimeTicks);
+            worldTime.StartWorldTime();
+            QuestManagerScript.Instance.LoadQuestProgress(saveData.questProgressData);
+            QuestManagerScript.Instance.handinQuests = saveData.handinQuestsID;
+            playerWalletScript.playerMoney = saveData.playerMoney;
+
+            equipmentScript.bagLevel = saveData.bagLevel;
+            equipmentScript.glovesLevel = saveData.glovesLevel;
+            equipmentScript.footwearLevel = saveData.footwearLevel;
+            equipmentScript.pickingToolLevel = saveData.toolLevel;
+            inventoryManager.inventorySize = saveData.inventorySize;
+            inventoryManager.setInventorySize();
+            playerMovementScript.movementSpeed = saveData.playerSpeed;
+            playerItemPickUpScript.holdDuration = saveData.pickUpSpeed;
+            playerItemPickUpScript.pickUpRange = saveData.pickUpRange;
+
+            Debug.Log("Game Loaded");
+        }
+        else
+        {
+            SaveGame();
+            worldTime.StartWorldTime();
+            inventoryManager.setInventorySize();
+            Debug.Log("No save file found, creating a new one.");
+        }
+        volumeSettings.LoadVolume();
+    }
+
+    public void DeleteSave()
+    {
+        if (File.Exists(saveLocation))
+        {
+            File.Delete(saveLocation);
+            Debug.Log("Save file deleted.");
+        }
+        else
+        {
+            Debug.Log("No save file to delete.");
+        }
+    }
+}
